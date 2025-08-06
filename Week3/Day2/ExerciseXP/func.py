3785bd0136d3d9054cfabbb87d618d952e90db2a
Matteo Balducci <matteob@wix.com>
2025-08-06 21:37:58 +0300

    Add Faker module exercise with user data generation

---
+++ Week3/Day2/ExerciseXP/func.py
@@ -0,0 +1,14 @@
+#Exercise 2: Import
+#Goal: Create a module with a function and import it into another file.
+#Create a func.py file with a function that sums two numbers and prints the result. Then, import and call the function from exercise_one.py.
+#Step 1: Create func.py
+#Create a file named func.py.
+#Define a function inside that file that takes two numbers as arguments, sums them, and prints the result.
+#Step 2: Create exercise_one.py
+#Create a file named exercise_one.py.
+#Import the function from func.py using one of the import syntaxes provided in the instructions.
+#Call the imported function with two numbers.
+
+
+def sum(a,b):
+    print(a + b)
