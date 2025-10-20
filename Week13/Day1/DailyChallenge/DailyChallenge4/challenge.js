2eca89f7e992a2608ea0c2f8ac66456af5f7d8d1
Matteo Balducci <matteob@wix.com>
2025-10-20 21:38:04 +0300

    Daily Challenge Task 4

---
+++ Week13/Day1/DailyChallenge/DailyChallenge4/challenge.js
@@ -0,0 +1,8 @@
+import { greet } from '../DailyChallenge1/greeting.js';
+import { message } from '../DailyChallenge2/colorful-message.js';
+import { displayContent } from '../DailyChallenge3/files/read-file.js';
+
+
+console.log(greet('Matteo'))
+message()
+displayContent()
